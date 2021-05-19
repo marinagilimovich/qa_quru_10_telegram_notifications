23ff7f7433ab3884920bf02d1eff1d9f619c153e
marinagilimovich <66743245+marinagilimovich@users.noreply.github.com>
2021-05-19 22:45:21 +0200

    added some tests

---
+++ src/test/java/tests/PositiveTests.java
@@ -0,0 +1,18 @@
+package tests;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class PositiveTests {
+    @Test
+    void some1Test() {
+        assertTrue(true);
+    }
+
+    @Test
+    void some2Test() {
+        assertTrue(true);
+    }
+
+}
