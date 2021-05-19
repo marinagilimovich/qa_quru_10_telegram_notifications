dbc14033e1169aeeb72070fae62ffda82e68d9f7
marinagilimovich <66743245+marinagilimovich@users.noreply.github.com>
2021-05-19 23:10:36 +0200

    added .jar

--- src/test/java/tests/NegativeTests.java
+++ src/test/java/tests/NegativeTests.java
@@ -6,7 +6,7 @@ import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class NegativeTests {
     @Test
-    void some3Test() {
+    void some1Test() {
         assertTrue(false);
     }
 }
